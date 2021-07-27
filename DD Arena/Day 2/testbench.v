209615d51502b8c5df1c2d7411611d1372ee5a50
AneeshKandi <85028192+AneeshKandi@users.noreply.github.com>
2021-07-27 14:36:46 +0530

    Create testbench.v

---
+++ DD Arena/Day 2/testbench.v
@@ -0,0 +1,18 @@
+// Code your testbench here
+module testbench;
+  wire d, e;
+  reg a, b, c;
+  
+  code1 M(d, e, a, b, c);
+   begin
+    $dumpfile("testbench.vcd");
+    $dumpvars(1);
+    a = 1'b0; b = 1'b0; c = 1'b0;
+    #100
+    $display("%b %b", d, e);
+    a = 1'b1; b = 1'b1; c = 1'b1;
+    #100
+    $display("%b %b", d, e);
+    $finish();
+  end
+endmodule
